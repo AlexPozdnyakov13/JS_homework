f4d044dca1c34d1528b0b5499b063762f5c1f54e
Alex Pozdnyakov <pozdnyakovav@mail.ru>
2023-03-10 11:40:04 +0300

    Added homework10

---
+++ data.js
@@ -0,0 +1,20 @@
+const dataInfo = `[
+{
+    "activity": "Fill out a basketball bracket",
+    "type": "recreational",
+    "participants": 1,
+    "price": 0,
+    "link":"https://www.boredapi.com/api/activity",
+    "key": "7806284",
+    "accessibility": 0.1
+  },
+  {
+    "activity": "Play a volleyball match with some friends",
+    "type": "social",
+    "participants": 4,
+    "price": 0,
+    "link":"https://www.boredapi.com/api/activity",
+    "key": "4306710",
+    "accessibility": 0.3
+  }
+]`;
